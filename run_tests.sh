23ee9459d8bcdcaf4086cff2ec5e34a7458cca55
marceloslo <marceloslo@users.noreply.github.com>
2022-01-27 18:35:13 -0300

    graph

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,7 @@
 for filename in ./test/*; do 
 	f=$(basename "$filename")
 	for i in {1..10}; do
-		./main.exe 100 50 50 200 "g" < "$filename" >> "./result/$f"
+		./main.exe 100 50 50 100 "g" < "$filename" >> "./result/$f"
 	done
 done
+
